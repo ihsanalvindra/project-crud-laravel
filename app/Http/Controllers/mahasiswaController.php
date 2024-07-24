f60863e28a612a39a959c7c371d79e50f14038ac
ihsanalvindra <ihsanalvindra@gmail.com>
2024-07-24 12:41:45 +0700

    "crud-laravel"

---
+++ app/Http/Controllers/mahasiswaController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\mahasiswa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class mahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $katakunci = $request->katakunci;
+        $jumlahbaris = 8;
+
+        // Debug statement untuk memastikan $katakunci diterima
+        // dd($katakunci);
+
+        if (strlen($katakunci)) {
+            $data = mahasiswa::where('nim', 'like', "%$katakunci%")
+                ->orWhere('nama', 'like', "%$katakunci%")
+                ->orWhere('jurusan', 'like', "%$katakunci%")
+                ->paginate($jumlahbaris);
+        } else {
+            $data = mahasiswa::orderby('nim', 'asc')->paginate($jumlahbaris);
+        }
+
+        return view('mahasiswa.index')->with('data', $data);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Session::flash('nim', $request->nim);
+        Session::flash('nama', $request->nama);
+        Session::flash('jurusan', $request->jurusan);
+        $request->validate([
+            'nim' => 'required|numeric|unique:mahasiswa,nim',
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ], [
+            'nim.required' => 'Perhatian ! NIM Wajib di isi',
+            'nim.numeric' => 'Perhatian ! NIM Wajib di isi angka',
+            'nim.unique' => 'Perhatian ! NIM sudah terdaftar',
+            'nama.required' => 'Perhatian ! Nama Wajib di isi',
+            'jurusan.required' => 'Perhatian ! Jurusan Wajib di isi',
+        ]);
+        $data = [
+            'nim' => $request->nim,
+            'nama' => $request->nama,
+            'jurusan' => $request->jurusan,
+        ];
+        mahasiswa::create($data);
+        return redirect()->to('mahasiswa')->with('success', 'Berhasil menambahkan data!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = mahasiswa::where('nim', $id)->first();
+        return view('mahasiswa.edit')->with('data', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ], [
+            'nama.required' => 'Perhatian ! Nama Wajib di isi',
+            'jurusan.required' => 'Perhatian ! Jurusan Wajib di isi',
+        ]);
+        $data = [
+            'nama' => $request->nama,
+            'jurusan' => $request->jurusan,
+        ];
+        mahasiswa::where('nim', $id)->update($data);
+        return redirect()->to('mahasiswa')->with('success', 'Berhasil memperbarui data!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        mahasiswa::where('nim', $id)->delete();
+        return redirect()->to('mahasiswa')->with('success', 'Berhasil menghapus data!');
+    }
+}
